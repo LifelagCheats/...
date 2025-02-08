afeece02617ad85cca690d54b7266cd527785b03
Laggy <HEHESUSSYAMOGUS2.0@gmail.com>
2025-02-08 15:39:55 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,9 +1,9 @@
 # Save this file as SetupAndRun.ps1 and run it with administrative privileges
 
 # CONFIGURATION VARIABLES
-$repoUrl  = "https://https://github.com/LifelagCheats/..."  # change this to your repo URL
-$repoName = "..."                                      # expected folder name after cloning
-$mainScript = "script.pyw"                                   # the script to execute inside the repo
+$repoUrl  = "https://github.com/LifelagCheats/..."  # Change this to your repo URL
+$repoName = "..."                                      # Expected folder name after cloning
+$mainScript = "script.pyw"                             # The script to execute inside the repo
 
 # Function to check if a command exists
 function Command-Exists {
@@ -19,8 +19,20 @@ Write-Host "Starting setup..."
 # 1. Install Git if not already installed
 if (-not (Command-Exists "git")) {
     Write-Host "Installing Git..."
-    winget install --id Git.Git -e --silent `
-        --accept-package-agreements --accept-source-agreements
+    $git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
+    $asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | Where-Object name -like "*64-bit.exe"
+    
+    if ($asset) {
+        $installer = "$env:temp\$($asset.name)"
+        Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
+        
+        $git_install_inf = "<install inf file>"
+        $install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=\"$git_install_inf\""
+        
+        Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+    } else {
+        Write-Host "Failed to fetch Git installer."
+    }
 } else {
     Write-Host "Git is already installed."
 }
@@ -28,8 +40,10 @@ if (-not (Command-Exists "git")) {
 # 2. Install Python if not already installed
 if (-not (Command-Exists "python")) {
     Write-Host "Installing Python..."
-    winget install --id Python.Python.3 -e --silent `
-        --accept-package-agreements --accept-source-agreements
+    Invoke-WebRequest -UseBasicParsing -Uri 'https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe' -OutFile 'c:/veera/python-3.11.0-amd64.exe'
+    Start-Process -FilePath 'c:/veera/python-3.11.0-amd64.exe' -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -Wait
+    setx /M path "%path%;C:\Program Files\Python311\"
+    $env:PATH = $env:PATH + ";C:\Program Files\Python311\"
 } else {
     Write-Host "Python is already installed."
 }
